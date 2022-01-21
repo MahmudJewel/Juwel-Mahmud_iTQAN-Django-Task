e1796e9f4d189c9b64a42b77b5b3782b8f43e64f
jewel <jmbengal.mbstu@gmail.com>
2022-01-22 03:42:58 +0600

    task 1 to completed

--- product/views.py
+++ product/views.py
@@ -3,9 +3,11 @@ from product import models as PMODEL
 # Create your views here.
 
 def categorywise_product_view(request, pk):
-    products = PMODEL.Product.objects.all()
+    products = PMODEL.Product.objects.filter(product_category=pk)
+    category = PMODEL.Product_category.objects.get(id=pk)
     context = {
         'products':products,
+        'category':category,
     }
     return render(request, 'product/categorywise_product.html', context)
 
