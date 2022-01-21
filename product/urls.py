9e00deecda30c4d05da9179ed080da5f5be69f30
jewel <jmbengal.mbstu@gmail.com>
2022-01-22 03:16:17 +0600

    single product view added

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from product import views
 
 urlpatterns = [
     path('/<int:pk>/product', views.categorywise_product_view, name=''),
+    path('/<int:pk>/single_product', views.single_product_view, name=''),
 ]
--- product/views.py
+++ product/views.py
@@ -8,3 +8,10 @@ def categorywise_product_view(request, pk):
         'products':products,
     }
     return render(request, 'product/categorywise_product.html', context)
+
+def single_product_view(request, pk):
+    product = PMODEL.Product.objects.get(id=pk)
+    context = {
+        'product': product,
+    }
+    return render(request, 'product/single_product_view.html', context)
