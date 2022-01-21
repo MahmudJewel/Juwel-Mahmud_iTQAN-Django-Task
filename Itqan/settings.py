a86e3eacda96f77f71421307df98b461bb1f18aa
jewel <jmbengal.mbstu@gmail.com>
2022-01-21 12:43:49 +0600

    nav & footer added

--- Itqan/settings.py
+++ Itqan/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # external app 
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Itqan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
