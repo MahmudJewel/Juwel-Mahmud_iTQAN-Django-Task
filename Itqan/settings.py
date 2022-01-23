1bb8ca2756927a8e234b22e6a6793b27804c96e3
jewel <jmbengal.mbstu@gmail.com>
2022-01-23 13:11:00 +0600

    setup for heroku

--- Itqan/settings.py
+++ Itqan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o=)d+v1-d4!c50)ah*c6zw!8hkelxq33*gizc6oz!vn(+(0d$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    #For Heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
